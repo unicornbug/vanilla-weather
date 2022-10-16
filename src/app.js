1e37b938690cd443e6a2201533f260a84756d8d5
unicornbug <veronika.djurica@outlook.com>
2022-10-16 20:53:49 +0100

    added unit conversion
    -removed forecast section of html

--- src/app.js
+++ src/app.js
@@ -11,10 +11,8 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let month = date.getMonth() + 1;
-  console.log(month);
-
   let weekDay = week[date.getDay()];
-  console.log(date);
+
   //formating date
   return ` ${weekDay}, ${date.getDate()}/${month}/${date.getFullYear()}`;
 }
@@ -37,6 +35,7 @@ function displayTemperature(response) {
     "#current-weather-description"
   );
   let elementCity = document.querySelector("#city");
+  let elementCountry = document.querySelector("#country");
   let elementFeelsLike = document.querySelector("#feels-like-element");
   let elementMaxT = document.querySelector("#maxT");
   let elementMinT = document.querySelector("#minT");
@@ -47,10 +46,13 @@ function displayTemperature(response) {
 
   let weatherDescription = response.data.weather[0].description;
   elementCity.innerHTML = response.data.name;
+  elementCountry.innerHTML = response.data.sys.country;
   elementFeelsLike.innerHTML = Math.round(response.data.main.feels_like);
   elementMaxT.innerHTML = Math.round(response.data.main.temp_max);
   elementMinT.innerHTML = Math.round(response.data.main.temp_min);
-  elementCurrentTemp.innerHTML = Math.round(response.data.main.temp);
+
+  celsiusTemperature = response.data.main.temp;
+  elementCurrentTemp.innerHTML = Math.round(celsiusTemperature);
   elementPressure.innerHTML = parseFloat(
     response.data.main.pressure / 1000
   ).toFixed(2);
@@ -93,3 +95,50 @@ findTown.addEventListener("submit", searchingTown);
 
 let magnifier = document.getElementById("magnifier");
 magnifier.addEventListener("submit", searchingTown);
+
+//Searching for local weather
+function showPosition(position) {
+  let lon = position.coords.longitude;
+  let lat = position.coords.latitude;
+  let apiKey = `fb99ccb8bab77cbda8d3a1f7be433a27`;
+  let openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  axios.get(openWeatherUrl).then(displayTemperature);
+}
+
+function findLocalWeather() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let button = document.getElementById("location");
+button.addEventListener("click", findLocalWeather);
+
+//converting temperature units (celsius to farenheit)
+//calculate F
+function calculateF(event) {
+  event.preventDefault();
+
+  let temperature = document.getElementById("current-temperature");
+  temperature.innerHTML = Math.round(celsiusTemperature * 1.8 + 32);
+  //injecting css (change of color)
+  let fahrenheit = document.querySelector("#f");
+  let celsius = document.querySelector("#c");
+  fahrenheit.style.color = "black";
+  celsius.style.color = "rgba(54, 56, 62, 0.4)";
+
+  celsius.addEventListener("click", calculateC);
+}
+
+function calculateC(event) {
+  event.preventDefault();
+  let temperature = document.getElementById("current-temperature");
+  temperature.innerHTML = Math.round(celsiusTemperature);
+
+  let celsius = document.querySelector("#c");
+  let fahrenheit = document.querySelector("#f");
+  celsius.style.color = "black";
+  fahrenheit.style.color = "rgba(54, 56, 62, 0.4)";
+}
+
+let celsiusTemperature = null;
+
+let fahrenheit = document.querySelector("#f");
+fahrenheit.addEventListener("click", calculateF);
